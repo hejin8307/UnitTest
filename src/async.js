fccdf3a5fda25ff0988621a448380646091ab005
limhyejin <hejin8307@naver.com>
2023-05-08 00:06:32 +0900

    organize repository

--- src/async.js
+++
@@ -1,8 +0,0 @@
-function fetchProduct(error) {
-  if (error === 'error') {
-    return Promise.reject('network error');
-  }
-  return Promise.resolve({item: 'Mike', price: 200});
-}
-
-module.exports = fetchProduct;
